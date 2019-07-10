c4a963f21836f81c9229a1db4ce0b41b2885729a
blck-snwmn <whitesnowmancreator@gmail.com>
2019-07-10 20:19:16 +0900

    update dump request for debug

--- HelloServer.go
+++ HelloServer.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	dump, _ := httputil.DumpRequest(r, true)
+	fmt.Println(string(dump))
 	fmt.Fprintf(w, "<html><body>hello world</body></html>")
 }
 
